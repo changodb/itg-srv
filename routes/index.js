eb2ef8cd388ee714cee909c91476259175165a3e
Lewis King <lewis.king@fauna.com>
2020-04-24 18:05:09 -0500

    Working POST endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,55 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-//Hardcoding state
-simfileResults: [
-  {
-    "songName": "My cool song",
-    "songArtist": "DJ SNAZZ",
-    "bpm": 69,
-    "packName": "Foobar",
-    "difficulty": {
-      "easy": 3
-    },
-    "expanded": false
-  },
-  {
-    "songName": "Freaknik is Bacc",
-    "songArtist": "T-Pain",
-    "bpm": 420,
-    "packName": "Rackem",
-    "difficulty": {
-      "oni": 13
-    },
-    "expanded": false
-  },
-  {
-    "songName": "Thizzle DanCE",
-    "songArtist": "Mac Dre",
-    "bpm": 111,
-    "packName": "Hoozawutzit",
-    "difficulty": {
-      "beginner": 1,
-      "light": 3,
-      "standard": 5,
-      "heavy": 8,
-      "challenge": 11,
-    },
-    "expanded": false
-  }
-]
-
-/* GET home page. 
+/* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log(req);
   res.render('index', { title: 'Fit Up Your Style' });
 });
-*/
 
 /* Create POST endpoint for passing search results to itg-frontend. */
-router.post('/search', function (req, res) {
+router.post('/', function (req, res) {
   //extract data from request
   //create a database entry?
-  res.json(simfileResults);
+  console.log(req.body);
+  res.status(200).json({message:'ok'});
 });
 
 module.exports = router;
