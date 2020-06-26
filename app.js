05d83764e891cac1c6ee21da2d70bfff1f2f4230
Gene Peters <gene@telligent-data.com>
2020-06-26 17:36:39 -0500

    Usesecrets (#18)
    * use secrets and deploy to cloud run
    
    * aadding an update-env-vars flag to set that this is production
    
    * reverting prefix
    
    * fixing an assignment

--- app.js
+++ app.js
@@ -17,10 +17,30 @@ const corsOptions = {
 
 const fs = require('fs')
 
+var mongoUrl;
 if (process.env.NODE_ENV !== 'production') {
    require('dotenv').config();
+   mongoUrl = process.env.MONGO_URL;
 }
-const mongoUrl = process.env.MONGO_URL;
+if (process.env.NODE_ENV === 'production') {
+   const {SecretManagerServiceClient} = require('@google-cloud/secret-manager');
+   const client = new SecretManagerServiceClient();
+
+   async function accessSecretVersion() {
+      const [version] = await client.accessSecretVersion({
+         name: 'projects/716644706008/secrets/mongo-url/versions/latest',
+      });
+   
+      // Extract the payload as a string.
+      const payload = version.payload.data.toString();
+      console.log(payload);
+      mongoUrl = payload;
+   }
+   
+   accessSecretVersion();
+}
+
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -35,7 +55,7 @@ app.use(function(req, res, next){
 
 // MAIN APP ROUTE ENTRYPOINT
 const routes = require('./routes');
-app.use('/api', routes);
+app.use(routes);
 
 
 module.exports = app;
