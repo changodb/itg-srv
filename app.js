b80fd8c698ac4c3f33173d3d2cc55eb4471dff7f
gene <gene@telligent-data.com>
2020-06-25 20:34:36 -0500

    merging frontend into backend

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var db = require('./mongo');
 const cors = require('cors');
 
 
-const indexRouter = require('./routes/index');
 const searchRouter = require('./routes/search');
 
 const app = express();
@@ -20,15 +19,10 @@ const fs = require('fs')
 const credsPath = './creds.json';
 const parsed = JSON.parse(fs.readFileSync(credsPath, 'UTF-8'));
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions))
 // Make our db accessible to our router
@@ -36,23 +30,12 @@ app.use(function(req, res, next){
     db.connect(parsed.mongoUrl, next);
 });
 
-app.use('/', indexRouter);
+// MAIN APP ROUTES
+// Static files should be served from root
+let _path = path.join(__dirname, 'frontend', 'build');
+console.log(_path);
+app.use(express.static(path.join(__dirname, 'frontend', 'build')));
 app.use('/search', searchRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
 
 module.exports = app;
--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  entry: './frontend/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'public/javascripts'),
-  },
-};
