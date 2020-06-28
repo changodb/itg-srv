1705bfc7ffdf60b32fdf9d2504700ad01e222105
changodb <chandler.wyatt@mongodb.com>
2020-06-27 19:45:20 -0500

    undo CORS fix 01

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ const cors = require('cors');
 
 const app = express();
 if (process.env.NODE_ENV === 'production') {
-   // var corsOrigin = ['https://fitupyourstyle.com', 'https://www.fitupyourstyle.com'];
-   var corsOrigin = 'https://fitupyourstyle.com';
+   var corsOrigin = ['https://fitupyourstyle.com', 'https://www.fitupyourstyle.com'];
 } else {
    var corsOrigin = 'http://localhost:3001';
 }
